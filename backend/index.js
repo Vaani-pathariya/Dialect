fe82fb444d14dc1dbca503f54716d83fd3166d0b
Vaani <vaanipathariya@gmail.com>
2023-02-25 03:53:11 +0530

    making the basic format

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,26 @@
 const express=require('express')
 const bodyParser=require('body-parser')
 const cors= require('cors')
+const mongoose =require('mongoose')
 const app=express();
 const port =5000;
 app.use(cors())
+mongoose.connect('mongodb+srv://vaani:vaani@cluster0.w1oeafk.mongodb.net/?retryWrites=true&w=majority',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+);
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+    console.log("Connected successfully");
+});
+
+// Configuring body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
 
 app.listen(port, () => console.log(`Hello world app listening on port ${port}!`))
---
+++ frontend/src/pages/Main.js
@@ -0,0 +1,11 @@
+
+const Main =()=>{
+    return(
+        <div>
+            <nav>
+                hello i am navbar
+            </nav>
+        </div>
+    )
+}
+export default Main;
